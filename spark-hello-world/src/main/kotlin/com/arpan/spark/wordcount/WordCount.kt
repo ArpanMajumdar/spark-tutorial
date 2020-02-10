1865ddd1c78a3c3413f2b47d8b308e87aca20cab
Arpan Majumdar <arpan.majumdar.dev@gmail.com>
2020-02-11 00:25:31 +0530

    First commit

---
+++ spark-hello-world/src/main/kotlin/com/arpan/spark/wordcount/WordCount.kt
@@ -0,0 +1,24 @@
+package com.arpan.spark.wordcount
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.api.java.JavaSparkContext
+import org.slf4j.LoggerFactory
+
+fun main() {
+
+    val logger = LoggerFactory.getLogger("WordCount")
+
+    val sparkConf = SparkConf().setAppName("word-count").setMaster("local[4]")
+    val sparkContext = JavaSparkContext(sparkConf)
+
+    val lines: JavaRDD<String> = sparkContext.textFile("input/word_count.text", 4)
+    val words = lines.flatMap { line ->
+        line.split(" ").listIterator()
+    }
+    val wordCounts: Map<String, Long> = words.countByValue()
+
+    wordCounts.forEach { (word, count) ->
+        logger.info("$word => $count")
+    }
+}
